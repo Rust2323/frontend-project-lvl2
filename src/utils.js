c774c697502f1adbba6327b64c069d94b18350ef
Rustem Ishmakov <dartrust@gmail.com>
2023-03-01 12:49:14 +0600

    created new function for building tree

--- src/utils.js
+++ src/utils.js
@@ -1,77 +1,49 @@
 /* eslint no-param-reassign: "error" */
-const makeString = (obj) => {
-  let newString = '{\n';
-  const keys = Object.keys(obj);
-  for (let i = 0; i < keys.length; i += 1) {
-    newString += `  ${keys[i]}: ${obj[keys[i]]}\n`;
-  }
-  newString += '}';
-  return newString;
-};
 
-const changeKeyName = (object, simbol = '') => {
-  const keys = Object.keys(object);
-  const result = keys.reduce((obj, key) => {
-    obj[`${simbol} ${key}`] = object[key];
-    return obj;
-  }, {});
+import _ from 'lodash';
+
+const buildTree = (obj1, obj2) => {
+  const keys1 = _.keys(obj1);
+  const keys2 = _.keys(obj2);
+  const unionKeys = _.sortBy(_.union(keys1, keys2));
+
+  const result = unionKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return {
+        name: key,
+        value: obj2[key],
+        type: 'added',
+      };
+    }
+    if (!_.has(obj2, key)) {
+      return {
+        name: key,
+        value: obj1[key],
+        type: 'deleted',
+      };
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return {
+        name: key,
+        type: 'nested',
+        children: buildTree(obj1[key], obj2[key]),
+      };
+    }
+    if (obj1[key] !== obj2[key]) {
+      return {
+        name: key,
+        value1: obj1[key],
+        value2: obj2[key],
+        type: 'changed',
+      };
+    }
+    return {
+      name: key,
+      value: obj1[key],
+      type: 'unchanged',
+    };
+  });
   return result;
 };
 
-const sort = (object) => {
-  const newKeys = [];
-  const keys = Object.keys(object);
-
-  for (let i = 0; i < keys.length; i += 1) {
-    newKeys.push(keys[i].slice(2));
-  }
-  const sortObj = newKeys.sort()
-    .reduce(
-      (obj, key) => {
-        for (let i = 0; i < keys.length; i += 1) {
-          const clearKey = keys[i].slice(2);
-          const keySimbol = keys[i].slice(0, 2);
-          if (key === clearKey && keySimbol === '  ') {
-            obj[`  ${key}`] = object[`  ${key}`];
-          } else if (key === clearKey && keySimbol === '- ') {
-            obj[`- ${key}`] = object[`- ${key}`];
-          } else if (key === clearKey && keySimbol === '+ ') {
-            obj[`+ ${key}`] = object[`+ ${key}`];
-          }
-        }
-
-        return obj;
-      },
-      {},
-    );
-  return sortObj;
-};
-
-const compare = (object1, object2) => {
-  const obj1 = changeKeyName(object1, '-');
-  const obj2 = changeKeyName(object2, '+');
-  const sum = { ...obj1, ...obj2 };
-  const objectsKeys = Object.keys(sum);
-  const result = {};
-  /* eslint-disable-next-line */
-  for (const key of objectsKeys) {
-    const cleanKey = key.slice(2);
-    if (!Object.hasOwn(object1, cleanKey)) {
-      result[key] = obj2[key];
-    } else if (!Object.hasOwn(object2, cleanKey)) {
-      result[key] = obj1[key];
-    } else if (object1[cleanKey] === object2[cleanKey]) {
-      const newKey = `  ${cleanKey}`;
-      result[newKey] = obj2[key];
-    } else if (Object.hasOwn(object1, cleanKey) && Object.hasOwn(object2, cleanKey)) {
-      if (key.slice(0, 1) === '-') {
-        result[key] = obj1[key];
-      } else {
-        result[key] = obj2[key];
-      }
-    }
-  }
-  return sort(result);
-};
-
-export { compare, makeString };
+export default buildTree;
