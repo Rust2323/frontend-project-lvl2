3b7be63a2caf4a465febdcf22ad02c9939890615
Rustem Ishmakov <dartrust@gmail.com>
2023-03-01 12:48:39 +0600

    added formatter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -9,7 +9,7 @@ program
 
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
-  // .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
